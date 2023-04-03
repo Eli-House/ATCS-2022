d195a07adeb2ffa9d841135f2cb6eca3ee20ac15
Eli-House <ethousenbold@gmail.com>
2023-04-02 23:58:37 -0700

    updated functions

--- twitter/models.py
+++ twitter/models.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 class User(Base):
+
     __tablename__ = "users"
 
     # Columns
@@ -25,6 +26,15 @@ class User(Base):
                              primaryjoin="User.username==Follower.following_id",
                              secondaryjoin="User.username==Follower.follower_id",
                              overlaps="following")
+    
+    tweets = relationship("Tweet", back_populates="user")
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return "@" + str(self.username)
 
 
 class Follower(Base):
@@ -35,14 +45,54 @@ class Follower(Base):
     follower_id = Column('follower_id', TEXT, ForeignKey('users.username'))
     following_id = Column('following_id', TEXT, ForeignKey('users.username'))
 
+    def __init__(self, follower_id, following_id):
+        self.follower_id = follower_id
+        self.following_id = following_id
+
 class Tweet(Base):
     # TODO: Complete the class
-    pass
+    __tablename__ = "tweets"
+
+    id = Column("id", INTEGER, primary_key = True)
+    content = Column("content", TEXT)
+    timestamp = Column("timestamp", TEXT, nullable=False)
+    username = Column("username", ForeignKey("users.username"))
+    user = relationship("User", back_populates="tweets")
+    tags = relationship("TweetTag", back_populates="tweets")
+
+    def __init__(self, content, timestamp, username):
+        self.content = content
+        self.timetamp = timestamp
+        self.username = username
+    
+    def __repr__(self):
+        return self.user + "\n" + self.content + "\n" + self.tags + "\n" + self.timestamp
 
 class Tag(Base):
     # TODO: Complete the class
-    pass
+    __tablename__ = "tags"
+
+    id = Column("id", INTEGER, primary_key = True)
+    content = Column("content", TEXT)
+    
+    def __init__(self, content):
+        self.content = content
+    
+    def __repr__(self):
+        return "#" + str(self.content)
+
+
 
 class TweetTag(Base):
     # TODO: Complete the class
-    pass
+    __tablename__ = "tweetTags"
+
+    id = Column("id", INTEGER, primary_key = True)
+    tweet_id = Column("tweet_id", ForeignKey("tweets.id"))
+    tag_id = Column("tag_id", ForeignKey("tags.id"))
+    tweets = relationship("Tweet", back_populates="tags")
+
+    def __init__(self, tweet_id, tag_id):
+        self.tweet_id = tweet_id
+        self.tag_id = tag_id
+
--- twitter/twitter.py
+++ twitter/twitter.py
@@ -3,6 +3,9 @@ from database import init_db, db_session
 from datetime import datetime
 
 class Twitter:
+
+    def __init__(self, current_user=None):
+        self.current_user = current_user
     """
     The menu to print once a user has logged in
     """
@@ -38,37 +41,154 @@ class Twitter:
     is guaranteed to be logged in after this function.
     """
     def register_user(self):
-        pass
 
+        registration = False
+        taken = False
+        users = db_session.query(User.username)
+
+        while registration == False:
+            uname = input("Create your twitter handle:\n")
+            passOne = input("Create your password:\n")
+            passTwo = input("Verify your password:\n")
+
+            for usernames in users:
+                if uname == usernames:
+                    print("Username is taken, try again.")
+                    taken = True
+                    break
+
+            if taken == False:
+                if passOne == passTwo:
+                    newUser = User(uname, passOne)
+                    db_session.add(newUser)
+                    db_session.commit()
+                    print("Welcome " + uname)
+                    registration = True
+                    self.current_user = uname
+                else: 
+                    print("Passwords dont match, try agian.")
+        
     """
     Logs the user in. The user
     is guaranteed to be logged in after this function.
     """
     def login(self):
-        pass
+        users = db_session.query(User.username, User.password)
+
+        username = input("Enter your username:\n")
+        password = input("Enter your password:\n")
+
+        login = False
+        for usernames, passwords in users:
+            if username == usernames and password == passwords:
+                print("Welcome " + usernames)
+                login = True
+                self.current_user = username
+                break
+        if login == False:
+            print("Invalid username or password")
 
     
     def logout(self):
-        pass
+        self.current_user = None
 
     """
     Allows the user to login,  
     register, or exit.
     """
     def startup(self):
-        pass
+
+        print("Select a menu option")
+        print("1. Log in")
+        print("2. Sign up")
+        print("3. Exit")
+
+        option = int(input(""))
+
+        if option == 1:
+            self.login()
+        elif option == 2:
+            self.register_user()
+        elif option == 3:
+            self.end()
 
     def follow(self):
-        pass
+        follow = input("Enter the username of the person you want to follow:\n")
+        users = db_session.query(User.username).first()
+        following = db_session.query(Follower.following_id).where(self.current_user == Follower.follower_id).first()
+        real_user = True
+        already_following = False
+
+        for people in following:
+            if follow == people:
+                print("You already follow " + follow)
+                already_following == True
+                break
+
+        if already_following == False:
+            for usernames in users:
+                if follow == str(usernames):
+                    print("you are now folling " + follow)
+                    new_follower = Follower(self.current_user, follow)
+                    db_session.add(new_follower)
+                    db_session.commit()
+                    real_user = True
+                    break
+
+        if real_user == False:
+            print("This user does not exits")
 
     def unfollow(self):
-        pass
+        unfollow = input("Enter the username of the person you want to unfollow:\n")
+        users = db_session.query(User.username).first()
+        following = db_session.query(Follower.following_id).where(self.current_user == Follower.follower_id).first()
+        real_user = True
+        already_following = True
+
+        for people in following:
+            if unfollow == people:
+                already_following == False
+                break
+        
+        if already_following == True:
+            for usernames in users:
+                if unfollow == str(usernames):
+                    print("you are now unfolling " + unfollow)
+                    unfollower = Follower(self.current_user, unfollow)
+                    db_session.delete(unfollower)
+                    db_session.commit()
+                    real_user = True
+                    break
+        else:
+            print("You don't follow " + unfollow)
+
+        if real_user == False:
+            print("This user does not exits")
 
     def tweet(self):
-        pass
-    
+        content = input("Enter the message you want to tweet: ")
+        tags = input("Enter tags (seperate by spaces): ")
+        timestamp = datetime.now()
+        new_tweet = Tweet(content, timestamp, self.current_user)
+        tag = db_session.query(Tag).all()
+
+        for hashtag in tags.split():
+            new_tag = True
+            for made_tag in tag:
+                if hashtag == made_tag:
+                    new_tag = False
+            if new_tag == True:
+                create_tag = Tag(hashtag) 
+                db_session.add(create_tag)
+        db_session.add(new_tweet)
+        db_session.commit()
+     
     def view_my_tweets(self):
-        pass
+        tweets = db_session.query(Tweet).where(Tweet.username == self.current_user)
+
+        for post in tweets:
+            self.print_tweets(post)
+            
     
     """
     Prints the 5 most recent tweets of the 
@@ -78,7 +198,19 @@ class Twitter:
         pass
 
     def search_by_user(self):
-        pass
+        users = db_session.query(User.username)
+        uname = input("Enter a username: \n")
+        real_user = False
+        
+        for usernames in users:
+            if uname == usernames:
+                real_user = True
+                tweets = db_session.query(Tweet).where(Tweet.username == uname)
+                break
+        if real_user == True:
+            self.print_tweets(uname)
+        if real_user == False:
+            print("No user has that name")
 
     def search_by_tag(self):
         pass
@@ -92,25 +224,28 @@ class Twitter:
 
         print("Welcome to ATCS Twitter!")
         self.startup()
+        active = True
+        while active == True:
+            self.print_menu()
+            option = int(input(""))
 
-        self.print_menu()
-        option = int(input(""))
-
-        if option == 1:
-            self.view_feed()
-        elif option == 2:
-            self.view_my_tweets()
-        elif option == 3:
-            self.search_by_tag()
-        elif option == 4:
-            self.search_by_user()
-        elif option == 5:
-            self.tweet()
-        elif option == 6:
-            self.follow()
-        elif option == 7:
-            self.unfollow()
-        else:
-            self.logout()
+            if option == 1:
+                self.view_feed()
+            elif option == 2:
+                self.view_my_tweets()
+            elif option == 3:
+                self.search_by_tag()
+            elif option == 4:
+                self.search_by_user()
+            elif option == 5:
+                self.tweet()
+            elif option == 6:
+                self.follow()
+            elif option == 7:
+                self.unfollow()
+            else:
+                self.logout()
+                active = False
         
         self.end()
+
